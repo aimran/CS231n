19186c0473a9e2c262d90ac2473003cdc2d19fec
Michael Estes <mjestes@oakland.edu>
2016-06-10 13:14:23 -0700

    Update to assignment 2

--- assignment2/cs231n/classifiers/fc_net.py
+++ assignment2/cs231n/classifiers/fc_net.py
@@ -45,7 +45,12 @@ class TwoLayerNet(object):
     # weights and biases using the keys 'W1' and 'b1' and second layer weights #
     # and biases using the keys 'W2' and 'b2'.                                 #
     ############################################################################
-    pass
+    
+    self.params['W1'] = np.random.randn(input_dim, hidden_dim) * weight_scale
+    self.params['b1'] = np.zeros(hidden_dim)
+    self.params['W2'] = np.random.randn(hidden_dim, num_classes) * weight_scale
+    self.params['b2'] = np.zeros(num_classes)
+
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -75,7 +80,11 @@ class TwoLayerNet(object):
     # TODO: Implement the forward pass for the two-layer net, computing the    #
     # class scores for X and storing them in the scores variable.              #
     ############################################################################
-    pass
+    
+    a2, relu_cache = affine_relu_forward(X, self.params['W1'], self.params['b1'])
+    z2, forward_cache = affine_forward(a2, self.params['W2'], self.params['b2'])
+    scores = z2
+
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -95,7 +104,28 @@ class TwoLayerNet(object):
     # automated tests, make sure that your L2 regularization includes a factor #
     # of 0.5 to simplify the expression for the gradient.                      #
     ############################################################################
-    pass
+    
+    exp_scores = np.exp(scores)
+    correct_class_scores = exp_scores[range(len(y)), y]
+
+    loss = np.mean(-np.log(correct_class_scores / np.sum(exp_scores, axis=1)))
+    loss += 0.5 * self.reg * (np.sum(self.params['W1'] ** 2) \
+            + np.sum(self.params['W2'] ** 2))
+
+    dscores = exp_scores / np.sum(exp_scores, axis=1)[:, np.newaxis]
+    dscores[range(len(y)), y] -= 1
+    dscores /= X.shape[0]
+
+    # Backpropagate through forward affine layer
+    dx, dw, db = affine_backward(dscores, forward_cache)
+    grads['W2'] = dw
+    grads['b2'] = db
+
+    # Backpropagate through forward affine with ReLU activation layer
+    dx, dw, db = affine_relu_backward(dx, relu_cache)
+    grads['W1'] = dw
+    grads['b1'] = db
+
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
